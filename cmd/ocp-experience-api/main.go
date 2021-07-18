70bf891d978467a94f3a34a9f6fc37f6b6511230
Arew <noobterr@yandex.ru>
2021-07-18 10:55:26 +0300

    Task-1 impl

---
+++ cmd/ocp-experience-api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, OZON! This is ocp-experience-api project!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ozoncp/ocp-experience-api
+
+go 1.16
